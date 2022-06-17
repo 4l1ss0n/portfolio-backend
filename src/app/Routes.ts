87577118a68c30013187fdf1a19356e12fbdb73d
Alisson-Py <alissilva1231@hotmail.com>
2022-06-17 19:04:27 -0300

    feat: add users endoints

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import UsersControllers from "./controllers/UsersControllers";
 
 const routes = Router();
+
+const User = new UsersControllers();
+
+routes.get("/v1/users", User.Index);
+routes.get("/v1/users/login", User.Login);
+routes.post("/v1/users/register", User.Register);
 
 
 export default routes;
