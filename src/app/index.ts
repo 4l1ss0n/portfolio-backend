32f90ab15ad251b33f440bb6067ad2338e95fefe
Alisson-Py <alissilva1231@hotmail.com>
2022-06-16 21:59:40 -0300

    feat: initializate .env and database

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
 import cors from "cors";
 import routes from "./Routes";
+import { config } from "dotenv";
+import Database from '../database/connection';
+
+config();
+
+Database.initialize().then(res => {
+  console.log("DB CONNECTED");
+}).catch(err => {
+  console.log(err);
+  process.exit();
+});
 
 const app = express();
 
