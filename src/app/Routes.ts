6fff3235bdd4f699eb75653ec4af20357e169c90
Alisson-Py <alissilva1231@hotmail.com>
2022-06-18 21:07:50 -0300

    feat: add projects endpoints

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
+import ProjectControllers from "./controllers/ProjectsControllers";
 import UsersControllers from "./controllers/UsersControllers";
 
 const routes = Router();
 
 const User = new UsersControllers();
+const Projects = new ProjectControllers();
 
 routes.get("/v1/users", User.Index);
 routes.get("/v1/users/login", User.Login);
 routes.post("/v1/users/register", User.Register);
 
+
+routes.get("/v1/projects", Projects.Index);
+routes.get("/v1/projects/:id", Projects.Show);
+routes.post("/v1/projects/create", Projects.Store);
+routes.put("/v1/projects/update", Projects.Update);
+routes.delete("/v1/projects/delete", Projects.Delete);
 
 export default routes;
