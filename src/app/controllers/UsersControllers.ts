b3485cb3116d57ac73714687939e1971ba6573d3
Alisson-Py <alissilva1231@hotmail.com>
2022-06-28 21:38:27 -0300

    feat: imcrements a basic user view, veriication if user alrealy exits and gen token to send to front-end

--- src/app/controllers/UsersControllers.ts
+++ src/app/controllers/UsersControllers.ts
@@ -3,6 +3,7 @@ import Database from "../../database/connection";
 import Users from "../models/UsersModels";
 import bcrypt from "bcrypt";
 import * as yup from "yup";
+import authGen from "../middlewares/authGen";
 
 
 interface UsersInputType {
@@ -16,7 +17,12 @@ class UsersControllers {
     try {
       const UserR = Database.getRepository(Users);
       const response = await UserR.find();
-      return res.status(200).json({response});
+      const users = response.map(user => ({
+        fName: user.firstName,
+        lName: user.lastName,
+        email: user.email
+      }));
+      return res.status(200).json(users);
     } catch (err) {
       return res.status(500).json({
         err
@@ -41,6 +47,14 @@ class UsersControllers {
 
       const UserR = Database.getRepository(Users);
 
+      const alrealyExist = await UserR.findOne({
+        where: {
+          email
+        }
+      })
+      
+      if (alrealyExist) return res.status(405).json({err: "email alrealy register"});
+
       const passwordHash = bcrypt.hashSync(password, 2);
 
       const user = UserR.create({
@@ -52,7 +66,7 @@ class UsersControllers {
 
       await UserR.save(user);
 
-      return res.status(200).json({id: user.id})
+      return res.status(201).json({id: user.id})
     } catch (err) {
       return res.status(500).json({
         err
@@ -82,7 +96,15 @@ class UsersControllers {
       if (!response) return res.status(404).json({err: "user not found with this email"});
       if (!bcrypt.compareSync(password, response.passwordHash)) return res.status(401).json({err: "incorrect passoword"});
 
-      return res.status(200).json({response});
+      return res.status(200).json({
+        fName: response.firstName,
+        email: response.email,
+        token: authGen({
+          email: response.email,
+          id: response.id,
+          userLevel: response.userLevel
+        })
+      });
     } catch (err) {
       return res.status(500).json({
         err
