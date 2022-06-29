3307db6ac991aa650068e6886da11d26700e6181
Alisson-Py <alissilva1231@hotmail.com>
2022-06-28 21:42:13 -0300

    feat: create project view file

---
+++ src/app/views/ProjectViews.ts
@@ -0,0 +1,29 @@
+import Project from "../models/ProjectsModels";
+
+
+interface ProjectInputType extends Project {};
+
+interface ProjectOutputType {
+    id: string;
+    title: string;
+    description: string;
+    githubUrl: string;
+    hostUrl: string | null;
+    img: string | null;
+}
+
+
+export function ProjectViewSingle(project: ProjectInputType): ProjectOutputType {
+    return {
+        id: project.id,
+        title: project.title,
+        description: project.description,
+        githubUrl: project.githubUrl,
+        hostUrl: project.hostUrl,
+        img: project.img
+    };
+};
+
+export function ProjectViewMany(projects: ProjectInputType[]): ProjectOutputType[] {
+    return projects.map(project => ProjectViewSingle(project));
+};
