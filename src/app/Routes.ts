d37c2753c87faba789744e9a1fb0173c4a036d93
Alisson-Py <alissilva1231@hotmail.com>
2022-06-28 21:32:14 -0300

    feat: imcrements jwt authorization

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ProjectControllers from "./controllers/ProjectsControllers";
 import UsersControllers from "./controllers/UsersControllers";
+import Authorization from "./middlewares/Authorization";
 
 const routes = Router();
 
@@ -14,8 +15,8 @@ routes.post("/v1/users/register", User.Register);
 
 routes.get("/v1/projects", Projects.Index);
 routes.get("/v1/projects/:id", Projects.Show);
-routes.post("/v1/projects/create", Projects.Store);
-routes.put("/v1/projects/update", Projects.Update);
-routes.delete("/v1/projects/delete", Projects.Delete);
+routes.post("/v1/projects/create",Authorization, Projects.Store);
+routes.put("/v1/projects/update",Authorization, Projects.Update);
+routes.delete("/v1/projects/delete",Authorization, Projects.Delete);
 
 export default routes;
