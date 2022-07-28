7c4634179ec595ca4df357f26dacf9b35c63a056
Alisson-Py <alissilva1231@hotmail.com>
2022-07-27 21:10:06 -0300

    deploy file

--- deploy/app/index.js
+++ deploy/app/index.js
@@ -8,12 +8,20 @@ const cors_1 = __importDefault(require("cors"));
 const Routes_1 = __importDefault(require("./Routes"));
 const dotenv_1 = require("dotenv");
 const connection_1 = __importDefault(require("../database/connection"));
+const cloudinary_1 = require("cloudinary");
 (0, dotenv_1.config)();
+const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
 connection_1.default.initialize().then(res => {
     console.log("DB CONNECTED");
 }).catch(err => {
     console.log(err);
     process.exit();
+});
+cloudinary_1.v2.config({
+    cloud_name: CLOUD_NAME,
+    api_key: API_KEY,
+    api_secret: API_SECRET,
+    secure: true
 });
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
